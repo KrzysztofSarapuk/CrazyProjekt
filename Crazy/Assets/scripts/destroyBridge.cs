9a9e8be8548d95bae33b919d6b221b7905fc77d9
Krzysztof2 <sarapuk2017@wp.pl>
2021-05-30 21:07:04 +0200

    destructable bridge

---
+++ Crazy/Assets/scripts/destroyBridge.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyBridge : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "weapon")
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
