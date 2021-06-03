8de37762c9dc3ae1352bd09d7a6ab99973a16878
Krzysztof2 <sarapuk2017@wp.pl>
2021-06-03 15:30:54 +0200

    frog update on level 2

---
+++ Crazy/Assets/scripts/frogJump.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frogJump : MonoBehaviour
+{
+    private Transform FrogPosition;
+    private Rigidbody2D FrogRigidbody;
+    public int Jump;
+    public int speed;
+    void Start()
+    {
+        FrogPosition = GetComponent<Transform>(); // acces to Frog position
+        FrogRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FrogJump() 
+    {
+        if (this.transform.localPosition.x < Jump )
+        {
+            //FrogRigidbody.AddForce(transform.up * 2);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
