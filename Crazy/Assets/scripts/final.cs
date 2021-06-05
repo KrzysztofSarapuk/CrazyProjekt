770970a8e8de38ca2d3c8be81208a0090713d65a
Krzysztof2 <sarapuk2017@wp.pl>
2021-06-05 18:01:23 +0200

    you win!

---
+++ Crazy/Assets/scripts/final.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class final : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(7);
+        }
+    }
+}
