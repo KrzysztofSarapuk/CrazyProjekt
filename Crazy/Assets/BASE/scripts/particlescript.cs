119e7ad4433bc0b88797ff253bb81f01c2e429bf
Reeposter <45016189+Reeposter@users.noreply.github.com>
2021-07-21 15:05:24 +0200

    Scripted Particles
    Dust Particle dodane pod playerem ze skryptem włączającym particle na jakiejkolwiek kolizji

---
+++ Crazy/Assets/BASE/scripts/particlescript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particlescript : MonoBehaviour
+{
+    [SerializeField] ParticleSystem Particles_Dust = null;
+
+    void OnTriggerEnter2D (Collider2D collision)   
+        {
+            DustParticle();
+            Debug.Log("trigger");
+        }
+    
+
+    public void DustParticle()
+    {
+        Particles_Dust.Play();
+    }
+}
