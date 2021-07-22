3d5f26045f7cf3eaa5772ef27b76ecbe1dc27a89
Krzysztof2 <sarapuk2017@wp.pl>
2021-07-22 22:22:05 +0200

    healing barrel and damage barrel

---
+++ Crazy/Assets/BASE/scripts/AddHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AddHealth : HealthBar
+{
+    public float heal_damage;
+
+    private Image pasek;  
+    public Text stateHealth;
+
+    public void Start()
+    {
+        pasek = HealthBar.FindObjectOfType<Image>(); 
+        pasek = healthBarFilling;
+        stateHealth.text = (pasek.fillAmount * 100).ToString() + " %";
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision) // damage 
+    {
+
+        if (collision.tag == "Player")
+        {
+            AddHealth(heal_damage); // minus or plus damage
+            stateHealth.text = (pasek.fillAmount * 100).ToString() + " %";
+            Destroy(this.gameObject);
+
+        }
+    }
+}
